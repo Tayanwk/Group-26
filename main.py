3b01350ac7433f4d8859e79e903c01290a95408c
Taya <taya@MBP-Taya.lan>
2022-01-16 23:33:18 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def calc():
+    a = 12
+    b = 15
+    c = a+b
+    return c
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    print("Resultat:", calc())
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Hello World!")
